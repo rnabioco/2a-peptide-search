43b43b2971f74063ea48698ff0a6f56e8a4ca24e
Jay Hesselberth <jay.hesselberth@gmail.com>
2024-01-26 06:22:38 -0700

    plot lit comparisons

--- results/2024-01-23/run.sh
+++ results/2024-01-23/run.sh
@@ -1,4 +1,8 @@
-zcat lit-annotations.tsv.gz  | grep -v '^#' | awk 'BEGIN {FS="\t"} {print ">"$1" " $2"\n"$3}' | gzip -c > lit-annotations.faa.gz
+zcat lit-annotations.tsv.gz  \
+    | grep -v '^#' \
+    | awk 'BEGIN {FS="\t"} {print ">"$1"|"NR"|"$2"\n"$3}' \
+    | sed 's/ /_/g' \
+    | gzip -c > lit-annotations.faa.gz
 
 hmmsearch --tblout lit-annotations.class-1.hmmsearch.tab ../../curated-models/2A-class-1.hmm lit-annotations.faa.gz | gzip -c > lit-annotations.class-1.hmmsearch.gz
 hmmsearch --tblout lit-annotations.class-2.hmmsearch.tab ../../curated-models/2A-class-2.hmm lit-annotations.faa.gz | gzip -c > lit-annotations.class-2.hmmsearch.gz
