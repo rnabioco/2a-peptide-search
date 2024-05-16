91cb3fdb7e23d4556d45e94572fd9b1c960743f7
Jay Hesselberth <jay.hesselberth@gmail.com>
2024-05-16 14:47:45 -0600

    finish fn analysis

--- results/2024-05-16/compare.py
+++ results/2024-05-16/compare.py
@@ -2,19 +2,46 @@
 #! /usr/bin/env python
 from Bio import AlignIO
 from Bio import SeqIO
+from Bio import SearchIO
+
+from collections import defaultdict
 
 import csv
 import sys
 import gzip
 import pdb
 
+ref_seqs = dict()
+row_num = 1
 for row in csv.DictReader(gzip.open("all-sequences.tsv.gz", "rt"), delimiter="\t"):
-    pdb.set_trace()
+    # id in the tsv, hmmer adds underscores
+    name = row['Name'].replace(" ", "_")
+    anal_id = row['Analysis ID']
+    # id in the fasta
+    name_fa = f"{name}|{row_num}|{anal_id}".replace('N/A', 'NA')
+    
 
-for record in SeqIO.parse(gzip.open(sys.argv[1], "rt"), "fasta"):
-    pdb.set_trace()
+    seq = row['Sequence']
+    status = row['Old/new']
 
-for aln in AlignIO.parse(sys.argv[2], "stockholm"):
-    for record in aln.alignment.sequences:
-        pass
+    ref_seqs[name_fa] = (name, seq, status)
+
+    row_num += 1
+
+hit_records = defaultdict(set)
+for result in SearchIO.parse("lit-annotations.class-1.hmmsearch.tab", "hmmer3-tab"):
+    for hit in result.hits:
+        if hit.domain_included_num == 1:
+            hit_records["class-1"].add(hit.id)
+for result in SearchIO.parse("lit-annotations.class-2.hmmsearch.tab", "hmmer3-tab"):
+    for hit in result.hits:
+        if hit.domain_included_num == 1:
+            hit_records["class-2"].add(hit.id)
+
+for ref_name, ref_info in ref_seqs.items():
+    for class_name, hits in hit_records.items():
+        if ref_name in hits:
+            print(ref_name, *ref_info, class_name, "found", sep = "\t")
+        else:
+            print(ref_name, *ref_info, class_name, "absent", sep = "\t")
 
--- results/2024-05-16/run.sh
+++ results/2024-05-16/run.sh
@@ -1,5 +1,6 @@
 gzcat all-sequences.tsv.gz  \
     | grep -v '^Name' \
+    | sed 's/N\/A/NA/g' \
     | awk 'BEGIN {FS="\t"} {print ">"$1"|"NR"|"$2"\n"$3}' \
     | sed 's/ /_/g' \
     | gzip -c > lit-annotations.faa.gz
@@ -15,3 +16,5 @@ hmmsearch \
   --tblout lit-annotations.class-2.hmmsearch.tab \
   ../../curated-models/2A-class-2.hmm lit-annotations.faa.gz \
   | gzip -c > lit-annotations.class-2.hmmsearch.gz
+
+python compare.py lit-annotations.class-1.hmmsearch.tab | cut -f4-6 | sort | uniq -c
