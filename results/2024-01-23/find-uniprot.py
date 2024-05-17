d3a6199b3cc23fb61d27ddb6c4dd8a3312fce11a
Jay Hesselberth <jay.hesselberth@gmail.com>
2024-05-17 11:09:09 -0600

    add some notes

--- results/2024-01-23/find-uniprot.py
+++ results/2024-01-23/find-uniprot.py
@@ -2,6 +2,8 @@
 
 import sys
 import gzip
+
+from pyfaidx import Fasta
 from collections import defaultdict
 
 lookup = defaultdict(set)
@@ -11,12 +13,20 @@ for line in gzip.open(sys.argv[1], 'rt'):
     if line.startswith("#"): continue
     fs = [f.strip() for f in line.split("\t")]
     
-    name, seq = fs[0], fs[1] 
-    lookup[seq].add(name)
+    name, seq, status = fs[0], fs[1], fs[3]
+
+    if status == 'Old':
+        lookup[seq].add(name)
 
 # check for unique
-for seq, names in lookup.items():
-    if len(names) > 1:
-        print(len(names), names, sep = "\t")
+#for seq, names in lookup.items():
+#    if len(names) > 1:
+#        print(len(names), names, sep = "\t")
 
+fasta = Fasta(sys.argv[2])
+fasta_ids = fasta.keys()
+for fasta_id in fasta_ids:
+    for seq in lookup:
+        if seq in fasta[fasta_id]:
+            print(fasta_id, seq, sep = "\t")
 
---
+++ results/NOTES.md
@@ -0,0 +1,6 @@
+## 2023-11-05
+
+Devi mentioned that Linda vD was asking whether bacteriophage encode 2A
+peptides. If that were true, it would indicate that 2A peptides work in 
+bacteria.
+
