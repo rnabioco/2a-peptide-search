5b58bdace3bf160c2eebd0dec4ec5eb23d450528
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-01-04 07:27:37 -0700

    more bodhi updates

--- data/ref/uniprot/reference_proteomes/dl.sh
+++ data/ref/uniprot/reference_proteomes/dl.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
 #BSUB -J dload-ref-proteomes
+#BSUB -eo logs/%J.errout
 
-wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Reference_Proteomes_2024_02.tar.gz
+wget -c https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Reference_Proteomes_2024_02.tar.gz
--- data/ref/uniprot/reference_proteomes/extract.sh
+++ data/ref/uniprot/reference_proteomes/extract.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
 #BSUB -J extract-ref
+#BSUB -eo logs/extract-ref.%J.errout
 
-tar zxvf Reference_Proteomes_2024_02.tar.gz --wildcards "*.fasta.gz" --exclude "*_DNA*"
+# tar zxvf Reference_Proteomes_2024_02.tar.gz --wildcards "*.fasta.gz" --exclude "*_DNA*"
+
+cat Bacteria/*/*.fasta.gz Archaea/*/*.fasta.gz \
+    Eukaryota/*/*.fasta.gz Viruses/*/*.fasta.gz \
+    > reference_proteomes.fasta.gz
